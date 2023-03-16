0b50c7ab2299bd1c1f4f08b77ad4d85a7839278b
Leon Ljunghorn <leonljunghorn@gmail.com>
2023-03-16 19:45:43 +0100

    Create

---
+++ welcome.php
@@ -0,0 +1,19 @@
+
+
+<b>Hello world.... >.< </b>
+
+<b>Fellow traveler,</b> this website was created by Leon Ljunghorn at <a href="https://12ants.com">12ants.com</a>.
+The theme <a href="https://github.com/12ants/blank4"><b>[blank4]</b></a> was tailored to Katla Aero's specifications. As well as a few plugins and addons to make this site functional and easy to maintain. 
+
+<hr>
+
+Most updates can be made through the Guthenberg editor without much tuning of the code. 
+
+<hr>
+
+But if the layout or functions do break please contact webadmin at <a href="mailto:leonljunghorn@gmail.com">leonljunghorn@gmail.com</a>
+
+<h2>Enjoy!</h2>
+
+
+<img width="400px" src="https://12ants.com/wp-content/uploads/harold-swamp.png.webp"></a> //localhost
