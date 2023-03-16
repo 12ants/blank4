87f20e67a1eab0585acd6c77ce2d4890dfa522a4
Leon Ljunghorn <leonljunghorn@gmail.com>
2023-03-16 20:09:33 +0100

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -1,19 +1,19 @@
 
 
-<b>Hello world.... >.< </b>
+<small><p><code>Hello world     >.<</code></p></small>
 
-<b>Fellow traveler,</b> this website was created by Leon Ljunghorn at <a href="https://12ants.com">12ants.com</a>.
-The theme <a href="https://github.com/12ants/blank4"><b>[blank4]</b></a> was tailored to Katla Aero's specifications. As well as a few plugins and addons to make this site functional and easy to maintain. 
 
-<hr>
-
+  
+<code><b>Fellow traveler,</b> this website was created by Leon Ljunghorn at <a href="https://12ants.com">12ants.com</a>.</code><hr><code>The theme <a href="https://github.com/12ants/blank4"><b>[blank4]</b></a> was tailored to Katla Aero's specifications. 
+As well as a few plugins and addons to make this site functional and easy to maintain. </code>
+<br><hr><br>
 Most updates can be made through the Guthenberg editor without much tuning of the code. 
-
-<hr>
-
+<br><hr><br>
 But if the layout or functions do break please contact webadmin at <a href="mailto:leonljunghorn@gmail.com">leonljunghorn@gmail.com</a>
 
 <h2>Enjoy!</h2>
-
-
-<img width="400px" src="https://12ants.com/wp-content/uploads/harold-swamp.png.webp"></a> //localhost
+<code>
+<br>*<br>*
+<img width="400px" src="https://12ants.com/wp-content/uploads/harold-swamp.png.webp"> </a>  //localhost
+<br>*<br>*
+</code>
