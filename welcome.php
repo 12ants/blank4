bdc1466446b67a0c837594f59767201267ed216e
Leon Ljunghorn <leonljunghorn@gmail.com>
2023-04-11 12:33:30 +0200

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -1,3 +1,23 @@
+<pre style="padding-bottom:0;margin:0;font-family:monospace">Hello contributer! </pre>
+
+
+##############
+
+
+
+ <h4><b>Fellow traveler,</b> this website was created by with help from <a href="https://12ants.com">12ants.com</a>.</h4><hr><h4>The theme <a href="https://github.com/12ants/blank4"><b>[blank4]</b></a> was tailored to Katla Aero's specifications. 
+As well as a few plugins and addons to make this site functional and easy to maintain. </h4>
+<br><hr><br>
+Most updates can be made through the Guthenberg editor without much tuning of the code. 
+<br><hr><br>
+But if the layout or functions do break please contact webadmin at <a href="mailto:leonljunghorn@gmail.com">admin@12ants.com</a>
+
+<h2>Enjoy!</h2>
+<h1>
+<img width="400px" src="https://12ants.com/wp-content/uploads/harold-swamp.png.webp"> </a>  //admin
+</h1>
+You can add any HTML to the welcome panel.
+
 
 
 <small><p><code>Hello world     >.<</code></p></small>
@@ -17,3 +37,5 @@ But if the layout or functions do break please contact webadmin at <a href="mail
 <img width="400px" src="https://12ants.com/wp-content/uploads/harold-swamp.png.webp"> </a>  //localhost
 <br>*<br>*
 </code>
+
+
